3f29c31c9d9361b4a0d836a98de016cd99d9aa56
Robert Williams <robert@a3marketing.com>
2020-05-07 20:34:53 -0500

    added dependencies

--- schema.sql
+++ schema.sql
@@ -1,3 +1,21 @@
 DROP DATABASE IF EXISTS restaurants;
 
 CREATE DATABASE restaurants;
+
+USE DATABASE restaurants;
+
+CREATE TABLE disliked
+(
+    id INT AUTO_INCREMENT,
+    disliked_name VARCHAR(255)
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE liked
+(
+    id INT AUTO_INCREMENT,
+    rest_name VARCHAR(255),
+    phone_number INT(255),
+    rest_address VARCHAR(255),
+    favorite BOOLEAN default false
+);
