990e78515459a9d8b3b22a787491f6e583613004
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-05-12 22:07:08 -0500

    configured aws config

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "dlubgk1zp0j1nw85",
+    "password": "pm5lykpwcc94p9sj",
+    "database": "wh9vaw41o85ds5ll",
+    "host": "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "port": 3306,
     "dialect": "mysql"
   }
