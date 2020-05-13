1e62167f38d99b1a3ac0d70c175804cd0a70061a
Robert Williams <robert@a3marketing.com>
2020-05-12 20:24:51 -0500

    removed db from server page

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ function haltOnTimeout(req, res, next) {
     if (!req.timedout) next();
   }
 
-db.sequelize.sync({ force: true }).then(function(){
+// db.sequelize.sync({ force: true }).then(function(){
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT);
     });
-});
+// });
