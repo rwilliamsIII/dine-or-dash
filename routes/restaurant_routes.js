5325562155ffdd94052c1bce0daf44393644689d
Robert Williams <robert@a3marketing.com>
2020-05-12 20:19:52 -0500

    added * route

--- routes/restaurant_routes.js
+++ routes/restaurant_routes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 require('dotenv').config();
 
 
+        router.get("*", function(req, res) {
+        res.render("index");
+        });
+
         router.get("/", function(req, res) {
                 res.render("index");
         });
