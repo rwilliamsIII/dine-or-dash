bc72f6f4b0e8dedd97e9092fa64e55e7edd84be1
Robert Williams <robert@a3marketing.com>
2020-05-16 11:19:13 -0500

    got card appending to page with restaurant info

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,5 +1,4 @@
-
-
+var bcrypt = require("bcryptjs");
 module.exports = function(sequelize, DataTypes) {
     var Restaurant = sequelize.define("Restaurant", {
         id: {
@@ -12,4 +11,22 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false
         }
     });
-}
+    var User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    User.prototype.validPassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+      };
+      
+    User.addHook("beforeCreate", function(user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+    return Restaurant, User;
+};
