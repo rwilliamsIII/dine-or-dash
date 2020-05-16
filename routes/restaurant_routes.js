518bcbb88c4fdc6da1c9ccca236297fff6177afa
Loki-Malone <60401309+Loki-Malone@users.noreply.github.com>
2020-05-16 10:29:51 -0500

    API routes for update & delete

--- routes/restaurant_routes.js
+++ routes/restaurant_routes.js
@@ -20,5 +20,26 @@ module.exports = function(app){
                         console.log(dbRestaurant);
             });
         });
+        app.delete("/api/restaurants/:id", function(req, res) {
+                db.Restaurant.delete({
+                        id: req.body.id,
+                        name: req.body.name
+                }).then(function(newRestaurant) {
+                        res.json(newRestaurant);
+                        console.log(newRestaurant);
+                        (err => res.status(500).json(err));
+                
+                 });
+        });
+        app.update("/api/resturants/:id", function(req, res) {
+                db.Restaurant.update({
+                        id: req.body.id,
+                        name: req.body.name
+                }).then(function(newRestaurant) {
+                        res.json(newRestaurant);
+                        console.log(newRestaurant);
+                        (err => res.status(404).json(err));
 
+                })
+        });
 }
